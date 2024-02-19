ee603a1b4133dc3e84cec94132d8baea35fbf6e8
zongkuoxiong <1101732819@qq.com>
2024-02-19 10:32:31 +0800

    11

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,49 +1,51 @@
-import { defineConfig } from 'vitepress'
-import { generateSidebar } from 'vitepress-sidebar';
+import {defineConfig} from 'vitepress'
+import {generateSidebar} from 'vitepress-sidebar';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "zongkx",
-  description: "zongkx",
-  ignoreDeadLinks: true,
-  themeConfig: {// https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Java', link: '/doc/java/' },
-      { text: 'middleware', link: '/doc/middleware/' },
-      { text: 'js', link: '/doc/js/' },
-      { text: 'ops', link: '/doc/ops/' },
-      { text: 'practice', link: '/doc/practice/' },
-      { text: 'db', link: '/doc/db/' }
-    ],
-    sidebar: generateSidebar([{
-      documentRootPath: '/doc/java/',
-      collapseDepth: 4,
-      resolvePath: '/doc/java/',
-    },{
-      documentRootPath: '/doc/middleware/',
-      resolvePath: '/doc/middleware/',
-      collapseDepth: 4,
-    },{
-      documentRootPath: '/doc/js/',
-      resolvePath: '/doc/js/',
-      collapseDepth: 4,
-    },{
-      documentRootPath: '/doc/ops/',
-      resolvePath: '/doc/ops/',
-      collapseDepth: 4,
-    },{
-      documentRootPath: '/doc/practice/',
-      resolvePath: '/doc/practice/',
-      collapseDepth: 4,
-    },{
-      documentRootPath: '/doc/db/',
-      resolvePath: '/doc/db/',
-      collapseDepth: 4,
-    }]),
+    title: "zongkx",
+    description: "zongkx",
+    ignoreDeadLinks: true,
+    themeConfig: {// https://vitepress.dev/reference/default-theme-config
+        nav: [
+            {text: 'java', link: '/doc/java/'},
+            {text: 'middleware', link: '/doc/middleware/'},
+            {text: 'js', link: '/doc/js/'},
+            {text: 'ops', link: '/doc/ops/'},
+            {text: 'practice', link: '/doc/practice/'},
+            {text: 'db', link: '/doc/db/'}
+        ],
+        search: {
+            provider: 'local'
+        },
+        sidebar: generateSidebar([{
+            documentRootPath: '/doc/java/',
+            collapseDepth: 4,
+            resolvePath: '/doc/java/',
+        }, {
+            documentRootPath: '/doc/middleware/',
+            resolvePath: '/doc/middleware/',
+            collapseDepth: 4,
+        }, {
+            documentRootPath: '/doc/js/',
+            resolvePath: '/doc/js/',
+            collapseDepth: 4,
+        }, {
+            documentRootPath: '/doc/ops/',
+            resolvePath: '/doc/ops/',
+            collapseDepth: 4,
+        }, {
+            documentRootPath: '/doc/practice/',
+            resolvePath: '/doc/practice/',
+            collapseDepth: 4,
+        }, {
+            documentRootPath: '/doc/db/',
+            resolvePath: '/doc/db/',
+            collapseDepth: 4,
+        }]),
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/vuejs/vitepress'}
+        ]
+    }
 })
--- index.md
+++ index.md
@@ -3,7 +3,7 @@
 layout: home
 
 hero:
-  name: "My"
+  name: ""
   text: ""
   tagline: ""
 #  actions:
