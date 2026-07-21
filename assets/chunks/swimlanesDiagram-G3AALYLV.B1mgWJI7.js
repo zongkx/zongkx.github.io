6f80f4ecb6e88019ea4e1c04e7dcc67e482ffb58
zongkx <zongkx@users.noreply.github.com>
2026-07-21 01:17:35 +0000

    deploy: 1c5251596f2f65e5b3f820e806d912eeea373303

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.B1mgWJI7.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.DoRJojUg.js";import{_ as a}from"../app.DaW833qq.js";import"./chunk-5VM5RSS4.CxWaghTf.js";import"./chunk-XXDRQBXY.CwTBMftS.js";import"./chunk-VR4S4FIN.DQZ6XPeQ.js";import"./chunk-32BRIVSS.fxtrUvgK.js";import"./channel.hc3_9pZs.js";import"./framework.CZW044RH.js";import"./theme.CowFlDER.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
